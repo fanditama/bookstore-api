0710665240ea301071a0ea0a9ec04e836484a959
Affandi Pratama <60037959+affandipratam@users.noreply.github.com>
2024-11-23 17:24:04 +0700

    Setup Testing untuk Register API

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,19 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::delete("delete from books");
+        DB::delete("delete from inventory_books");
+        DB::delete("delete from banks");
+        DB::delete("delete from payments");
+        DB::delete("delete from users");
+    }
 }
