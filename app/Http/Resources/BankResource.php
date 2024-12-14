a2e8e2ba12065d5d7939cf0cc56f51965b3c6414
Affandi Pratama <afandip911@gmail.com>
2024-12-14 08:49:38 +0700

    Setup Controller, Request, Resource, dan URL untuk data Bank API

---
+++ app/Http/Resources/BankResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BankResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'account_number' => $this->account_number,
+            'account_name' => $this->account_name,
+            'image' => $this->image
+        ];
+    }
+}
